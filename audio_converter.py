803175246712ae9d3e849ac2a099cc16d658271c
Olexandr Bielinskiy <belinskiy2007@gmail.com>
2024-11-05 09:54:09 +0200

    Update audio_converter.py

--- audio_converter.py
+++ audio_converter.py
@@ -1,4 +1,13 @@
 from pydub import AudioSegment 
+import os
+
+# Укажите путь к FFmpeg (замените на ваш путь)
+ffmpeg_path = "C:\\ffmpeg\\bin"  # для Windows
+# ffmpeg_path = "/usr/local/bin"  # для macOS/Linux
+
+os.environ["PATH"] += os.pathsep + ffmpeg_path
+AudioSegment.converter = os.path.join(ffmpeg_path, "ffmpeg.exe")  # для Windows
+# AudioSegment.converter = os.path.join(ffmpeg_path, "ffmpeg")  # для macOS/Linux
 
 def converter_audio(input_file, output_format):
   audio = AudioSegment.from_file(input_file)
